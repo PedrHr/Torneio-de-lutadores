8b38b75136bb444381dd956a65654547abecd55e
Pedro Souza <pedrohenriquefcsouza@gmail.com>
2026-05-19 12:53:59 -0300

    Init commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,5 @@
 class Main {
     public static void main() {
-        int[] vet = new int[10];
-        for (int i = 0; i < vet.length; i++) {
-            vet[i] = i;
-        }
-        for(int i : vet){
-            System.out.println(i);
-            System.out.println(1);
-        }
+
     }
 }
